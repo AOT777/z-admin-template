9abdfd8682df2227ff7a6d84f46fbdad2a36370d
“你的名字” <>
2022-06-14 00:15:57 +0800

    品牌管理页part1制作完成

---
+++ src/views/product/TradeMark/index.vue
@@ -0,0 +1,212 @@
+<template>
+  <div>
+    <el-button type="primary" icon="el-icon-plus" style="margin-left: 5px" @click="showDialog(null)"
+      >添加</el-button
+    >
+    <el-table style="width: 100%" border :data="list">
+      <el-table-column type="index" label="序号" width="80px" align="center">
+      </el-table-column>
+      <el-table-column prop="tmName" label="品牌名称" width="width">
+      </el-table-column>
+      <el-table-column prop="prop" label="品牌logo" width="width">
+        <template slot-scope="{ row }">
+          <img
+            v-if="row.logoUrl === '/static/default.jpg'"
+            style="width: 80px; height: 80px"
+            src="@/assets/laoba.png"
+            alt=""
+          />
+          <img
+            v-else
+            :src="row.logoUrl"
+            style="width: 80px; height: 80px"
+            alt=""
+          />
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="操作" width="width">
+        <template slot-scope="{row}">
+          <el-button type="warning" icon="el-icon-edit" size="mini" @click="showDialog(row)"
+            >修改</el-button
+          >
+          <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteTradeMark(row)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- @size-change="handleSizeChange"
+      @current-change="handleCurrentChange" -->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="getPageList"
+      style="margin-top: 5px; text-align: center"
+      :current-page="1"
+      :page-count="5"
+      :page-sizes="[3, 5, 10]"
+      :page-size="3"
+      layout="prev, pager, next, jumper,->,sizes,total"
+      :total="31"
+    >
+    </el-pagination>
+    <el-dialog :title="tmForm.id?'修改品牌':'添加品牌'" :visible.sync="dialogFormVisible">
+      <el-form style="width:80%" ref="ruleForm" :model="tmForm" :rules="rules">
+        <el-form-item label="品牌名称" label-width="100px" prop="tmName">
+          <el-input v-model="tmForm.tmName" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌名称" label-width="100px" prop="logoUrl">
+          <el-upload
+            class="avatar-uploader"
+            action="/dev-api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload">
+            <img v-if="tmForm.logoUrl" :src="tmForm.logoUrl" class="avatar">
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addOrUpdateTradeMark"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TradeMark",
+  data() {
+    return {
+      page: 1,
+      limit: 3,
+      total: 0,
+      list: [],
+      dialogFormVisible:false,
+      tmForm:{
+        tmName:'',
+        logoUrl:''
+      },
+      rules:{
+        tmName: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'change' }
+        ],
+        logoUrl: [
+          { required: true, message: '请选择图片' }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.getPageList();
+  },
+  methods: {
+    async getPageList(pager = 1) {
+      this.page = pager;
+      const { page, limit } = this;
+      const result = await this.$API.trademark.reqTradeMarkList(page, limit);
+      // console.log(result);
+      if (result.code === 200) {
+        this.total = result.data.total;
+        this.list = result.data.records;
+      }
+    },
+    handleSizeChange(pageS) {
+      this.limit = pageS;
+      this.getPageList();
+    },
+    handleAvatarSuccess(res, file) {
+      this.tmForm.logoUrl=res.data
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    },
+    showDialog(row){
+      this.dialogFormVisible=true
+      for(let i in this.tmForm){
+        this.tmForm[i]=''
+      }
+      if(row===null){
+        return
+      }else{
+        this.tmForm={...row}
+      }
+    },
+    addOrUpdateTradeMark(){
+      this.$refs.ruleForm.validate(async (success)=>{
+        if(success){
+          this.dialogFormVisible = false
+          const result=await this.$API.trademark.reqAddOrUpdateTradeMark(this.tmForm)
+          if(result.code===200){
+            this.$message({message: this.tmForm.id?'修改品牌成功':'添加品牌成功',type: 'success'})
+            this.getPageList(this.tmForm.id?this.page:1)
+          }
+        }else{
+          console.log('error submit!!');
+          return false;
+        }
+      })
+    },
+    deleteTradeMark(row) {
+      this.$confirm(`确认删除${row.tmName}吗?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        const result=await this.$API.trademark.reqDeleteTradeMark(row.id)
+        if(result.code===200){
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+          this.getPageList(this.list.length>1?this.page:this.page-1)
+        }
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });          
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
